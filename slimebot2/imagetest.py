95bcce32dbd3b620c232c4364b9fdf4fda1d2d26
Charles Mogan <charlesdmogan@gmail.com>
2020-04-01 14:37:17 -0500

    fixed animated gif typo, added more slimes, added suport for exif rotation

--- slimebot2/imagetest.py
+++ slimebot2/imagetest.py
@@ -2,6 +2,7 @@ import os
 import urllib.parse
 import urllib.request
 from urllib.error import HTTPError
+import random
 
 from urllib.request import Request, urlopen
 import shutil
@@ -12,7 +13,6 @@ import cairo
 from PIL import Image, ImageDraw, ImageFilter
 from shutil import copyfileobj
 from cairosvg import svg2png
-
 
 
 def get_file_name(url: str):
@@ -33,25 +33,24 @@ def slime_image(url: str):
     req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     with urllib.request.urlopen(req) as response, open(file_name, 'wb') as out_file:
         copyfileobj(response, out_file)
-
+    os.system(f"gm mogrify -auto-orient {file_name}")
     im1 = Image.open(file_name)
 
     width, height = im1.size
-
-    os.system(f"inkscape -z -e ./images/result/slime1.png -w {width} -h {height} ./images/src/slime1.svg")
-    if im1.format == "GIFr":
+    slime_svg_path = get_random_slime_image()
+    os.system(f"inkscape -z -e ./images/result/slime.png -w {width} -h {height} {slime_svg_path}")
+    if im1.format == "GIF":
         if im1.is_animated:
             result_file = "./images/result/result.gif"
-            os.system(f"convert {file_name} -coalesce null: ./images/result/slime1.png -gravity center -layers "
+            os.system(f"convert {file_name} -coalesce null: ./images/result/slime.png -gravity center -layers "
                       f"composite "
                       f"{result_file}")
 
     else:
         result_file = "./images/result/result.png"
-        os.system(f"gm composite ./images/result/slime1.png {file_name} {result_file}")
+        os.system(f"gm composite ./images/result/slime.png {file_name} {result_file}")
     os.remove(file_name)
     return result_file
-
 
 
 def valid_image_url(url: str):
@@ -63,6 +62,7 @@ def valid_image_url(url: str):
         req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         meta_info = urlopen(req).info()
     except ValueError:  # for when it isn't a url at all
+        print(f"{e}")
         return False
     except HTTPError as e:  # mostly concerned about 403 forbiden errors
         print(f"{e}")
@@ -73,6 +73,16 @@ def valid_image_url(url: str):
     return False
 
 
+def get_random_slime_image():  # https://stackoverflow.com/questions/701402/best-way-to-choose-a-random-file-from-a-directory
+    n = 0
+    random.seed();
+    for root, dirs, files in os.walk('./images/src'):
+        for name in files:
+            n = n + 1
+            if random.uniform(0, n) < 1:
+                slime_image_path = os.path.join(root, name)
+    return slime_image_path
+
 # im2 = Image.open('./images/slime1.png')
 # im = Image.alpha_composite(im1, im2)
 # im.save("./images/result", "png")
@@ -82,4 +92,3 @@ def valid_image_url(url: str):
 # cairosvg.svg2png(
 #     file_obj=open("./images/slime1.svg", "rb"), scale=1,
 #     write_to="./images/slime1.png")
-
