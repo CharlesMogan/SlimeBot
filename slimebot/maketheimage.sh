b3d1c767cb41844f66832f0363534cfc91570669
Cha <charlesdmogan@gmail.com>
2025-05-24 21:59:29 -0500

    removed old slimebot

--- slimebot/maketheimage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-rm -f ./imaages/result.png;
-inkscape -z -e ./images/slime1.png -w $1 -h $2 ./images/slime1.svg;
-gm composite ./images/slime1.png ./images/taco.png ./images/result.png;
-rm -f ./images/slime1.png;
-rm -f ./images/taco.png;
-sleep 3 &
-wait %1
